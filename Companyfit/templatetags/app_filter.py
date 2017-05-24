a629029024db2fa8208f92077f24b2524fe893b2
= <=>
2017-05-25 00:19:20 +0530

    caps, date and sentiment_to_text filter added

--- Companyfit/templatetags/app_filter.py
+++ Companyfit/templatetags/app_filter.py
@@ -1,12 +1,30 @@
 from django  import template
-
+from DateTime import DateTime
+import string 
 register = template.Library()
 
 @register.filter (name = "get_from_dict")
 def keyvalue(dict, key):    
     return dict[key] 
 
-@register.filter(name = "get_inner_dict")
+@register.filter(name ="sentiment_filter")
+def get_sentiment(sentiment):
+	sentiment = str(sentiment)
+	options = { '-2' : 'Very Negative ' , '-1' : 'Negative ' , '0' : 'Neutral' , '1' : 'Positive ' , '2' : 'Very Positive' }
+	# print("present sentiment is : " + sentiment + " and returning " + options[sentiment])
+	return options[sentiment]	
 
-def keyvalue(dict,key1,key2):
-	return dict[key1][key2]
+
+@register.filter(name="date_filter")
+def get_date(date):
+	date = str(date)
+	x = DateTime(date)
+	new_date = ""
+	new_date += x.Day() + '  ' + str(x.day()) + '  ' + x.Month()  + '  ' + str(x.year())
+	print("new date is : " + str(new_date))
+	return new_date
+
+
+@register.filter(name = "caps_filter")
+def get_caps(word):
+	return string.capwords(word)
